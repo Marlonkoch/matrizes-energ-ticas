1e4b1708cb5f3d2e10fb13de918d1d0b60f3e8df
Marlonkoch <160977577+Marlonkoch@users.noreply.github.com>
2024-11-26 02:33:59 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Exemplo de JavaScript simples para interações (poderia incluir mais funcionalidades)
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sua mensagem foi enviada com sucesso!');
+});
